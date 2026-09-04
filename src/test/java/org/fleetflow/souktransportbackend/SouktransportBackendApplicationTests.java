6a14620f6ce28b7dee4d383b1c0fd73e99c0f4a4
soufiane <soufianeelbasraoui0@gmail.com>
2026-09-04 16:36:20 +0200

    update

--- src/test/java/org/fleetflow/souktransportbackend/SouktransportBackendApplicationTests.java
+++ src/test/java/org/fleetflow/souktransportbackend/SouktransportBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SouktransportBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
