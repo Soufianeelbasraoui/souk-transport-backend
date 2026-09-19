782cf10a57134b066d04586657ce0935503484c1
soufiane <soufianeelbasraoui0@gmail.com>
2026-09-19 14:59:59 +0200

    modifier TrajetServiceImpl

--- src/main/java/org/fleetflow/souktransportbackend/service/impl/TrajetServiceImpl.java
+++ src/main/java/org/fleetflow/souktransportbackend/service/impl/TrajetServiceImpl.java
@@ -19,8 +19,6 @@ import org.fleetflow.souktransportbackend.service.TrajetService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
